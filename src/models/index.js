2abc3c57478647266d9c562f85bae361eef86a2e
Anna Riabova <annaua04@gmail.com>
2023-02-04 17:25:02 +0200

    sponsors-v2

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const Notices = require('./NoticesModel');
 const News = require('./news');
+const Sponsors = require('./sponsors');
 
-module.exports = { Notices, News };
+module.exports = { Notices, News, Sponsors };
