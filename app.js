afeb3418df92a166af5104153ad98933ffdbca3e
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2023-02-04 14:11:55 +0200

    connected swagger

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+require('dotenv').config();
 
 console.clear();
 
-const usersRouter = require("./src/routes/api/users");
-const petsRouter = require("./src/routes/api/pets");
-const newsRouter = require("./src/routes/api/news");
-const sponsorsRouter = require("./src/routes/api/sponsors");
+const usersRouter = require('./src/routes/api/users');
+const petsRouter = require('./src/routes/api/pets');
+const newsRouter = require('./src/routes/api/news');
+const sponsorsRouter = require('./src/routes/api/sponsors');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-
-app.use("/api/users", usersRouter);
-app.use("/api/pets", petsRouter);
-app.use("/api/news", newsRouter);
-app.use("/api/sponsors", sponsorsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/news', newsRouter);
+app.use('/api/sponsors', sponsorsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
   // eslint-disable-next-line no-unused-vars
-  const { status = 500, message = "server error" } = err;
+  const { status = 500, message = 'server error' } = err;
   res.status(status).json({ message: err.message });
 });
 
