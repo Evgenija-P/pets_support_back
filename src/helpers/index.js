09d3334d652ee61be111a39310ba825f8d4a9e58
Evgenija-P <e.a.poduzova@gmail.com>
2023-02-04 22:55:51 +0200

    handleMongooseError

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,9 @@
-const HttpError = require("./HttpError");
+const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
-const hendleMongooseError = require('./hendleMongooseError')
+const handleMongooseError = require('./handleMongooseError');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
-  hendleMongooseError
+  handleMongooseError,
 };
