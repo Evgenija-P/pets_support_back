ea4140aab0873bd99239c4afdf76a1241185f938
Alexander Khardin <khporter1984@gmail.com>
2023-02-04 15:56:15 +0300

    add upload middleware to api

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,9 +1,18 @@
 const multer = require('multer');
 const path = require('path');
 
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, '..', '..', 'temp'),
+  filename: (req, file, cb) => {
+    const { _id: userId } = req.user;
+    const [name, extention] = file.originalname.split('.');
 
-const upload = (req,res, next) => {
-    console.log('upload middleware was invoked');
-}
+    const fileName = `user_${userId}_${name}_${Date.now()}.${extention}`;
+
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage });
 
 module.exports = upload;
