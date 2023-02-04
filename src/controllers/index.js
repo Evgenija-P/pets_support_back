522a7680a12e4e769dc57a5de2296ef158755312
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-04 13:49:00 +0200

    t

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,23 @@
 const news = require('./news');
 const sponsors = require('./sponsors');
+const {
+  getNoticesByCategory,
+  addNewNoticesController,
+  noticesByIdController,
+  deleteNoticesController,
+  addToFavoriteNoticesController,
+  getFavoriteNotices,
+  removeFromFavoriteNotices,
+} = require('./noticesControllers');
 
 module.exports = {
   news,
   sponsors,
+  getNoticesByCategory,
+  addNewNoticesController,
+  noticesByIdController,
+  deleteNoticesController,
+  addToFavoriteNoticesController,
+  getFavoriteNotices,
+  removeFromFavoriteNotices,
 };
