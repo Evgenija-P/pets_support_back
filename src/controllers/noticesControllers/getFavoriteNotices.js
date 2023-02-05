23c0228595b9d9b620eb129abf67284a7bd2ffbf
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2023-02-06 00:24:27 +0200

    Update getFavoriteNotices.js

--- src/controllers/noticesControllers/getFavoriteNotices.js
+++ src/controllers/noticesControllers/getFavoriteNotices.js
@@ -6,13 +6,7 @@ const getFavoriteNotices = async (req, res, next) => {
   //   const id = req.params.contactId;
   //   const categoryName = req.params.categoryName;
 
-  const { page = 1, limit = 8 } = req.query;
-  const skip = (page - 1) * limit;
-
-  const noticesList = await Notices.find({ favorite: true }, '', {
-    skip,
-    limit,
-  });
+  const noticesList = await Notices.find({ favorite: true });
   res.json({ message: noticesList });
   // };
   // module.exports = { getNoticesByCategory: ctrlWrapper(getNoticesByCategory) };
