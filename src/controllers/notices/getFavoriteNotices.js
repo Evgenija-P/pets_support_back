11ca3995b37f5d7b9ef4ca1abc29e320ce40a5ec
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-06 13:53:54 +0200

    finish

--- src/controllers/notices/getFavoriteNotices.js
+++ src/controllers/notices/getFavoriteNotices.js
@@ -1,12 +1,14 @@
 const { Notices, Favorite } = require('../../models');
-// const { ctrlWrapper } = require('../../../middlewares');
 const { HttpError } = require('../../helpers');
+
+const PER_PAGE = 20;
 
 const getFavoriteNotices = async (req, res, next) => {
   const {
     user: { _id: owner },
   } = req;
-
+  const { page = 1, limit = PER_PAGE } = req.query;
+  const skip = (page - 1) * limit;
   const isHaveFavorite = await Favorite.findOne({ owner });
   if (isHaveFavorite === null) {
     throw HttpError(404, `user don't have favorite notices `);
@@ -17,9 +19,16 @@ const getFavoriteNotices = async (req, res, next) => {
     throw HttpError(404, `user don't have favorite notices `);
   }
 
-  const favoriteNoticesList = await Notices.find({
-    _id: { $in: [...favoriteList] },
-  });
+  const favoriteNoticesList = await Notices.find(
+    {
+      _id: { $in: [...favoriteList] },
+    },
+    '',
+    {
+      skip,
+      limit,
+    }
+  );
   res.json({ message: favoriteNoticesList });
 };
 module.exports = getFavoriteNotices;
