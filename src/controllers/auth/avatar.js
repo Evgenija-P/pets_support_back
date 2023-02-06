59c7ca24959f47e8f63d0df5433dd9cabb88dc10
Lavrov Andrey <Sattarlav@gmail.com>
2023-02-06 17:09:14 +0200

    did controller avatar

--- src/controllers/auth/avatar.js
+++ src/controllers/auth/avatar.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_API_SECRET } = process.env;
-const {User} = require('../../models')
+const { User } = require('../../models');
+const fs = require('fs').promises;
 
 
 // Configuration
@@ -22,6 +23,9 @@ const avatar = async (req, res) => {
     const result = await cloudinary.uploader.upload(path, options);
     
     await User.findByIdAndUpdate(_id, { avatarURL: result.url });
+    
+    await fs.unlink(path);
+
 
     res.json({
         status: "Success",
