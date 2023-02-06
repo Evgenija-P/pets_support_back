d9e98244f535c806f1313d4048ad90a71e7bcee0
Lavrov Andrey <Sattarlav@gmail.com>
2023-02-06 15:28:19 +0200

    update controller avatar

--- src/controllers/auth/avatar.js
+++ src/controllers/auth/avatar.js
@@ -1,7 +1,36 @@
+const cloudinary = require('cloudinary').v2;
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_API_SECRET } = process.env;
+const {User} = require('../../models')
 
+
+// Configuration
+cloudinary.config({
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_API_KEY,
+    api_secret: CLOUD_API_SECRET,
+});
+
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
 
 const avatar = async (req, res) => {
+    const { path } = req.file;
+    const { _id } = req.user;
+    const result = await cloudinary.uploader.upload(path, options);
+    
+    await User.findByIdAndUpdate(_id, { avatarURL: result.url });
 
+    res.json({
+        status: "Success",
+        code: 200,
+        data: {
+            avatarURL: result.url,
+        }
+    })
+      
 }
 
 module.exports = avatar;
