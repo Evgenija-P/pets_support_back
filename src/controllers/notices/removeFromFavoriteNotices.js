0fa1cb7f8f583b59751cdab7c991f7dcb82f2057
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-13 06:17:21 +0200

    Update removeFromFavoriteNotices.js

--- src/controllers/notices/removeFromFavoriteNotices.js
+++ src/controllers/notices/removeFromFavoriteNotices.js
@@ -15,7 +15,8 @@ const removeFromFavoriteNotices = async (req, res, next) => {
   const newFavoriteList = prevFavoriteList.filter(
     notices => notices._id.toString() !== idNotices
   );
-  const favoriteUpdated = await Favorite.findOneAndUpdate(
+  // const favoriteUpdated = await Favorite.findOneAndUpdate(
+  await Favorite.findOneAndUpdate(
     { owner },
     {
       $set: { favoriteList: [...newFavoriteList] },
@@ -23,7 +24,7 @@ const removeFromFavoriteNotices = async (req, res, next) => {
     { new: true }
   );
 
-  res.status(201).json({ message: favoriteUpdated });
+  res.status(201).json({ message: idNotices });
 };
 
 module.exports = removeFromFavoriteNotices;
