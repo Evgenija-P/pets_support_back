491a47d3c29a4ffeef9244ced512254521ff02d0
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-18 02:49:42 +0200

    Update deleteNotices.js

--- src/controllers/notices/deleteNotices.js
+++ src/controllers/notices/deleteNotices.js
@@ -1,5 +1,16 @@
-const { Notices } = require('../../models');
+const { Notices, Favorite } = require('../../models');
 const { HttpError } = require('../../helpers');
+
+const updateFavoriteList = async ({ _id, list }) => {
+  const result = await Favorite.findOneAndUpdate(
+    { _id },
+    {
+      $set: { favoriteList: [...list] },
+    },
+    { new: true }
+  );
+  return result;
+};
 
 const deleteNotices = async (req, res, next) => {
   const idNotices = req.params.noticesId;
@@ -7,6 +18,36 @@ const deleteNotices = async (req, res, next) => {
   const {
     user: { _id: owner },
   } = req;
+  console.log('id not', idNotices);
+  const AllUsersFavorite = await Favorite.find();
+  const list = [];
+  AllUsersFavorite.forEach(element => {
+    const { favoriteList } = element;
+    const index = favoriteList.findIndex(
+      item => item._id.toString() === idNotices
+    );
+
+    if (index !== -1) {
+      list.push(element);
+    }
+  });
+  if (list.length > 0) {
+    list.forEach(element => {
+      const { _id, favoriteList } = element;
+      if (favoriteList.find(item => item._id === idNotices) !== -1) {
+        const updatedFavoritelist = favoriteList.filter(
+          item => item._id.toString() !== idNotices
+        );
+
+        try {
+          updateFavoriteList({ _id, list: updatedFavoritelist });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  }
+
   const noticesRemoved = await Notices.findOneAndRemove({
     owner,
     _id: idNotices,
